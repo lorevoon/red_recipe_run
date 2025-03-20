7533a43115d6fe60e2fb725fdf49fdf7758ba63a
Kayla Xu <kx2561@princeton.edu>
2025-03-20 15:53:02 +0800

    player with tool

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -2,6 +2,53 @@ using UnityEngine;
 
 public class PlayerController : Singleton<PlayerController>
 {
-    // TODO you can do player input stuff here! :D
-    
+    private float playerSpeed = 10; // speed player moves
+    private float toolRotateSpeed = 3;
+    public GameObject tool;
+
+    void Start() {
+        if (tool == null)
+            tool = GameObject.FindWithTag("Tool");
+        
+    }
+  
+    void Update () {
+        Move(); // Player Movement 
+    }
+  
+    void Move()
+    {
+        if (tool == null) return; // Ensure the tool is not null before moving it
+
+        if (Input.GetKey("up")) // Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+            //tool.transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+        
+        if (Input.GetKey("down")) // Press down arrow key to move backward on the Y AXIS
+        {
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            // tool.transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+        }
+        
+        if (Input.GetKey("left")) // Press left arrow key to move left on the X AXIS
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+            // tool.transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("right")) // Press right arrow key to move right on the X AXIS
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+            // tool.transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            // Rotate the tool to a specific angle (e.g., 45 degrees around the Z-axis)
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, 45f);
+            tool.transform.rotation = Quaternion.RotateTowards(tool.transform.rotation, targetRotation, toolRotateSpeed * Time.deltaTime);
+        }
+    }
 }
