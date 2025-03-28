4d48ba3b45dd84c159b8305c31b0ab9f9e55eece
Zoe <flyingdonuuts@gmail.com>
2025-03-29 00:34:03 +0800

    organize everyone's code

---
+++ Assets/_Scripts/Shaders&Lantern/LanternController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal; // Make sure to include this namespace
+
+public class LanternController : MonoBehaviour {
+    [SerializeField] private Light2D _light2D; // Reference to the Light2D component
+    private TimeController _timeController;
+
+    private bool _isLightOn = false; // Track the state of the light
+
+    void Start()
+    {
+        _timeController = TimeController.Instance;
+        _light2D.enabled = false;
+    }
+    
+    void Update() {
+        // Check if the 'Z' key is pressed
+        if (Input.GetKeyDown(KeyCode.Z) && _timeController.IsNight) {
+            _isLightOn = !_isLightOn; // Toggle the state of the light
+            _light2D.enabled = _isLightOn; // Enable or disable the Light2D component
+        }
+    }
+}
