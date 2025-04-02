308461dcdd31e6427a4bf8ab51e13a0711d4d243
Kayla Xu <kx2561@princeton.edu>
2025-04-02 14:23:14 +0800

    fixing the ingredient orientation and scaling basket, set max ingredients = 25

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,84 +1,89 @@
 using UnityEngine;
 using System.Collections.Generic; // Add this line to fix the error
 
+
 public class PlayerController : Singleton<PlayerController>
 {
-    private PlayerInventory _playerInventory;
-    private AudioSource _audioSource;
-    
-    public GameObject Tool;
-    public GameObject Basket;
-    
-    private float _playerSpeed = 10; // speed player moves
-    private float _toolSpeed = 3;
-
-    void Start()
-    {
-        _playerInventory = GetComponent<PlayerInventory>();
-        _audioSource = GetComponent<AudioSource>();
-        
-        if (Tool == null)
-            Tool = GameObject.FindWithTag("Tool");
-    }
+   private PlayerInventory _playerInventory;
+   private AudioSource _audioSource;
   
+   public GameObject Tool;
+   public GameObject Basket;
+  
+   private float _playerSpeed = 10; // speed player moves
+   private float _toolSpeed = 3;
+
+
+   void Start()
+   {
+       _playerInventory = GetComponent<PlayerInventory>();
+       _audioSource = GetComponent<AudioSource>();
+      
+       if (Tool == null)
+           Tool = GameObject.FindWithTag("Tool");
+   }
     void Update() {
-        Move();
-        HandlePickup();
-    }
+       Move();
+       HandlePickup();
+   }
 
-    void HandlePickup()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            // Try to pick up an ingredient
-            _playerInventory.TryPickUpIngredient();
-            
-        }
 
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            _playerInventory.DropIngredient();
-            
-        }
-    }
-  
+   void HandlePickup()
+   {
+       if (Input.GetKeyDown(KeyCode.Z))
+       {
+           // Try to pick up an ingredient
+           _playerInventory.TryPickUpIngredient();
+          
+       }
+
+
+       if (Input.GetKeyDown(KeyCode.X))
+       {
+           _playerInventory.DropIngredient();
+          
+       }
+   }
     void Move()
-    {
-        if (Tool == null) return; // Ensure the Tool is not null before moving it
+   {
+       if (Tool == null) return; // Ensure the Tool is not null before moving it
 
-        if (Input.GetKey("down")) // Press up arrow key to move forward on the Y AXIS
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0); // Left
-        }
-        
-        if (Input.GetKey("up")) // Press down arrow key to move backward on the Y AXIS
-        {
-            transform.eulerAngles = new Vector3(0, 0, 180); // Left
-        }
-        
-        if (Input.GetKey("right")) // Press left arrow key to move left on the X AXIS
-        {
-            transform.eulerAngles = new Vector3(0, 0, 90); // Right
-        }
-        
-        if (Input.GetKey("left")) // Press right arrow key to move right on the X AXIS
-        {
-            transform.eulerAngles = new Vector3(0, 0, -90); // Left
-        }
 
-        // Then handle movement if any arrow key is pressed
-        if (Input.GetKey("down") || Input.GetKey("up") || 
-            Input.GetKey("right") || Input.GetKey("left"))
-        {
-            // Move in the direction the player is facing
-            transform.Translate(Vector3.up * -_playerSpeed * Time.deltaTime, Space.Self);
-        }
+       if (Input.GetKey("down")) // Press up arrow key to move forward on the Y AXIS
+       {
+           transform.eulerAngles = new Vector3(0, 0, 0); // Left
+       }
+      
+       if (Input.GetKey("up")) // Press down arrow key to move backward on the Y AXIS
+       {
+           transform.eulerAngles = new Vector3(0, 0, 180); // Left
+       }
+      
+       if (Input.GetKey("right")) // Press left arrow key to move left on the X AXIS
+       {
+           transform.eulerAngles = new Vector3(0, 0, 90); // Right
+       }
+      
+       if (Input.GetKey("left")) // Press right arrow key to move right on the X AXIS
+       {
+           transform.eulerAngles = new Vector3(0, 0, -90); // Left
+       }
 
-        if (Input.GetKeyDown("space"))
-        {
-            // Rotate the Tool to a specific angle (e.g., 45 degrees around the Z-axis)
-            Quaternion targetRotation = Quaternion.Euler(0f, 0f, 45f);
-            Tool.transform.rotation = Quaternion.RotateTowards(Tool.transform.rotation, targetRotation, _toolSpeed * Time.deltaTime);
-        }
-    }
+
+       // Then handle movement if any arrow key is pressed
+       if (Input.GetKey("down") || Input.GetKey("up") ||
+           Input.GetKey("right") || Input.GetKey("left"))
+       {
+           // Move in the direction the player is facing
+           transform.Translate(Vector3.up * -_playerSpeed * Time.deltaTime, Space.Self);
+       }
+
+
+       if (Input.GetKeyDown("space"))
+       {
+           // Rotate the Tool to a specific angle (e.g., 45 degrees around the Z-axis)
+           Quaternion targetRotation = Quaternion.Euler(0f, 0f, 45f);
+           Tool.transform.rotation = Quaternion.RotateTowards(Tool.transform.rotation, targetRotation, _toolSpeed * Time.deltaTime);
+       }
+   }
 }
--- Assets/_Scripts/Player/PlayerInventory.cs
+++ Assets/_Scripts/Player/PlayerInventory.cs
@@ -1,70 +1,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerInventory : MonoBehaviour
 {
-    public List<GameObject> InventoryList = new List<GameObject>();
+   public List<GameObject> InventoryList = new List<GameObject>();
 
-    private PlayerController _playerController;
-    private float _pickupRadius = 2f; // Radius for picking up ingredients
 
-    public void AddItem(GameObject ingredient)
-    {
-        InventoryList.Add(ingredient);
-    }
+   private PlayerController _playerController;
+   private float _pickupRadius = 2f; // Radius for picking up ingredients
+   private int _max_ingredients = 25;
+   private int _curr_ingredients;
 
-    public GameObject RemoveItem()
-    {
-        GameObject ingredient = InventoryList[^1];
-        InventoryList.RemoveAt(InventoryList.Count - 1);
-        return ingredient;
-    }
 
-    public bool IsIngredientInInventory(GameObject ingredient)
-    {
-        return InventoryList.Contains(ingredient);
-    }
-    
-    
-    public void TryPickUpIngredient()
-    {
-        Collider2D[] nearbyIngredients = Physics2D.OverlapCircleAll(transform.position, _pickupRadius);
-        GameObject closestIngredient = null;
-        float closestDistance = Mathf.Infinity;
+   public void AddItem(GameObject ingredient)
+   {
+       InventoryList.Add(ingredient);
+   }
 
-        foreach (Collider2D col in nearbyIngredients)
-        {
-            if (col.CompareTag("Ingredient") && !InventoryList.Contains(col.gameObject))
-            {
-                float distance = Vector2.Distance(transform.position, col.transform.position);
-                if (distance < closestDistance)
-                {
-                    closestDistance = distance;
-                    closestIngredient = col.gameObject;
-                }
-            }
-        }
 
-        if (closestIngredient != null)
-        {
-            PickUpIngredient(closestIngredient); // pick up ingredient
-        }
-    }
+   public GameObject RemoveItem()
+   {
+       GameObject ingredient = InventoryList[^1];
+       InventoryList.RemoveAt(InventoryList.Count - 1);
+       return ingredient;
+   }
 
-    public void PickUpIngredient(GameObject ingredient)
-    {
-        AddItem(ingredient);
-        ingredient.transform.SetParent(PlayerController.Instance.Basket.transform);
-        ingredient.GetComponent<Collider2D>().enabled = false;
-        ingredient.GetComponent<SpriteRenderer>().enabled = false;
-    }
 
-    public void DropIngredient()
-    {
-        GameObject mostRecent = RemoveItem();
+   public bool IsIngredientInInventory(GameObject ingredient)
+   {
+       return InventoryList.Contains(ingredient);
+   }
+  
+  
+   public void TryPickUpIngredient()
+   {
+       Collider2D[] nearbyIngredients = Physics2D.OverlapCircleAll(transform.position, _pickupRadius);
+       GameObject closestIngredient = null;
+       float closestDistance = Mathf.Infinity;
 
+
+       foreach (Collider2D col in nearbyIngredients)
+       {
+           if (col.CompareTag("Ingredient") && !InventoryList.Contains(col.gameObject))
+           {
+               float distance = Vector2.Distance(transform.position, col.transform.position);
+               if (distance < closestDistance)
+               {
+                   closestDistance = distance;
+                   closestIngredient = col.gameObject;
+               }
+           }
+       }
+
+
+       if (closestIngredient != null && _curr_ingredients < _max_ingredients)
+       {
+           PickUpIngredient(closestIngredient); // pick up ingredient
+           _curr_ingredients += 1;
+       }
+   }
+
+
+   public void PickUpIngredient(GameObject ingredient)
+   {
+       AddItem(ingredient);
+
+
+       Transform basketTransform = PlayerController.Instance.Basket.transform;
+       basketTransform.localScale *= 1.05f; // Increase basket size by 5%
+
+
+       ingredient.transform.SetParent(basketTransform, true);
+       ingredient.GetComponent<Collider2D>().enabled = false;
+       ingredient.GetComponent<SpriteRenderer>().enabled = false;
+   }
+
+
+   public void DropIngredient()
+   {
+       GameObject mostRecent = RemoveItem();
+
+
+       // Store the world position before unparenting
+        Vector3 worldPosition = mostRecent.transform.position;
+
+        // Unparent the object
         mostRecent.transform.SetParent(null);
-        mostRecent.GetComponent<SpriteRenderer>().enabled = true;
-        mostRecent.GetComponent<Collider2D>().enabled = true;
-    }
+
+        // Restore the world position
+        mostRecent.transform.position = worldPosition;
+
+        // Ensure the rotation is reset to avoid unwanted rotation from the parent
+        mostRecent.transform.rotation = Quaternion.identity;
+
+
+       mostRecent.GetComponent<SpriteRenderer>().enabled = true;
+       mostRecent.GetComponent<Collider2D>().enabled = true;
+   }
 }
